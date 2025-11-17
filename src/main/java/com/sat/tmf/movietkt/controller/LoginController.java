6afeb7250904123443fdd872e5ee9f0a4a9a7566
Lalam Bhavana <bhavanalalam4@gmail.com>
2025-11-17 21:28:22 +0530

    Initial commit

---
+++ src/main/java/com/sat/tmf/movietkt/controller/LoginController.java
@@ -0,0 +1,58 @@
+package com.sat.tmf.movietkt.controller;
+
+import com.sat.tmf.movietkt.entities.User;
+import com.sat.tmf.movietkt.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    // =================== LOGIN PAGE ===================
+    @GetMapping
+    public String showLoginPage(Model model) {
+        model.addAttribute("pageTitle", "Login");
+        model.addAttribute("contentPage", "/WEB-INF/views/login.jsp");
+        return "layout/layout";
+    }
+
+    // =================== LOGIN SUBMIT ===================
+    @PostMapping
+    public String processLogin(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            HttpSession session,
+            Model model) {
+
+        // Fetch user by username
+        User user = userService.findByUsername(username);
+
+        // Validate user credentials
+        if (user != null && user.getPassword().equals(password)) {
+            session.setAttribute("loggedInUser", user);
+            model.addAttribute("message", "Welcome, " + user.getUsername() + "!");
+            model.addAttribute("pageTitle", "Movies");
+            model.addAttribute("contentPage", "/WEB-INF/views/movies.jsp");
+            return "layout/layout";
+        } else {
+            model.addAttribute("error", "Invalid username or password!");
+            model.addAttribute("pageTitle", "Login");
+            model.addAttribute("contentPage", "/WEB-INF/views/login.jsp");
+            return "layout/layout";
+        }
+    }
+
+    // =================== LOGOUT ===================
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/sat/tmf/movietkt/controller/UserMovieController.java
@@ -0,0 +1,56 @@
+package com.sat.tmf.movietkt.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sat.tmf.movietkt.entities.Movie;
+import com.sat.tmf.movietkt.entities.Show;
+import com.sat.tmf.movietkt.service.MovieService;
+import com.sat.tmf.movietkt.service.ShowService;
+
+@Controller
+public class UserMovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @Autowired
+    private ShowService showService;
+
+    // User browsing movies
+    @GetMapping("/movies")
+    public String listMoviesForUser(@RequestParam(required = false) String search,
+                                    @RequestParam(required = false) String language,
+                                    Model model) {
+
+        List<Movie> movies = (search != null && !search.isEmpty())
+                ? movieService.searchMovies(search)
+                : movieService.findAllMovies();
+
+        model.addAttribute("movies", movies);
+        model.addAttribute("search", search);
+        model.addAttribute("contentPage", "/WEB-INF/views/user/movies.jsp");
+        model.addAttribute("pageTitle", "Now Showing");
+        return "layout/layout";
+    }
+
+    // User movie showtimes
+    @GetMapping("/movies/{id}/shows")
+    public String listShowsForMovie(@PathVariable Integer id, Model model) {
+
+        Movie movie = movieService.findById(id);
+        List<Show> shows = showService.findUpcomingShows(movie);
+
+        model.addAttribute("movie", movie);
+        model.addAttribute("shows", shows);
+        model.addAttribute("contentPage", "/WEB-INF/views/user/movieShows.jsp");
+        model.addAttribute("pageTitle", movie.getTitle() + " - Showtimes");
+        return "layout/layout";
+    }
+}
