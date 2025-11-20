833dd0dc2c7c823351ad837870015ff8cfdf7c31
bhavanalalam <bhavanalalam4@gmail.com>
2025-11-20 07:55:40 +0530

    Update SeatTemplateService.java

--- src/main/java/com/sat/tmf/movietkt/service/SeatTemplateService.java
+++ src/main/java/com/sat/tmf/movietkt/service/SeatTemplateService.java
@@ -1,18 +1,23 @@
 package com.sat.tmf.movietkt.service;
 
-
 import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sat.tmf.movietkt.entities.Screen;
 import com.sat.tmf.movietkt.entities.SeatTemplate;
 
 public interface SeatTemplateService {
+
     SeatTemplate createTemplate(SeatTemplate template);
     SeatTemplate updateTemplate(SeatTemplate template);
     SeatTemplate findById(Integer id);
+
+    @Transactional   
     List<SeatTemplate> findAll();
+
+    @Transactional   
     List<SeatTemplate> findByScreen(Screen screen);
+
     void deleteTemplate(Integer id);
 }
-
-
